01c7834bb0d5d3b7a151379a32d3df2ee1634c7e
@Inori <2665611998@qq.com>
2020-05-17 17:36:32 +0800

    u

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -681,17 +681,6 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
-
-      // 打包后屏蔽log
-      isEnvProduction &&
-        new webpack.optimize.UglifyJsPlugin({
-          compress: {
-            warnings: false,
-            drop_debugger: true,
-            drop_console: true
-          },
-          sourceMap: true
-        })
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.

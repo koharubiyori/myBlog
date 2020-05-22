028a0d31735d133cd0cd3734f99e2341112e209a
Inori <2665611998@qq.com>
2020-05-22 15:36:35 +0800

    u

--- src/views/article/view/utils/trimArticleContent.ts
+++ src/views/article/view/utils/trimArticleContent.ts
@@ -1,5 +1,16 @@
+import qs from 'qs'
+import createRouter from '~/utils/createRouter'
+
 export default function trimArticleContent(articleRootElement: HTMLElement): void{
+  const router = createRouter()
+  
   articleRootElement.querySelectorAll('a').forEach(aTag =>{
-    if(/^https?:\/\//.test(aTag.href)) aTag.setAttribute('target', '_blank')
+    aTag.addEventListener('click', e => {
+      // 判断是否为外链，如果是则直接跳出，如果为内链，则分别解析path和params，之后传给路由器
+      if(/^https?:\/\//.test(aTag.getAttribute('href')!)) { return }
+      e.preventDefault()
+      const [path, params] = aTag.getAttribute('href')!.split('?')
+      router.push(path as any, { search: qs.parse(params) })
+    })
   })
 }
